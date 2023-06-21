d0fbc9aa530b07bce719438d5b1ad680397e1773
Iván Arriola <ivan.arriola@fing.edu.uy>
2023-06-21 00:20:54 -0300

    utils

--- setearBarrios.R
+++ setearBarrios.R
@@ -9,6 +9,7 @@ library(dplyr)
 library(DBI)
 library(RPostgres)
 library(sf)
+source(here::here("utils.R"))
 
 con <- DBI::dbConnect(
   RPostgres::Postgres(),
@@ -25,35 +26,6 @@ d_sensores <- DBI::dbGetQuery(
 )
 
 mvd_map <- st_read(here::here("./ine_barrios/ine_barrios_mvd_nbi85.shp"))
-
-transformarCoord <- function(lat, lon, mvd_map){
-  puntos_lat_lng <- data.frame(lng = lon, lat = lat)
-  puntos_sf <- st_as_sf(puntos_lat_lng,
-                        coords = c("lng", "lat"),
-                        crs = 4326)
-  puntos_transformados <- st_transform(puntos_sf,
-                                       crs = st_crs(mvd_map))
-  return(puntos_transformados)
-}
-
-encontrar_barrio <- function(lat, lon, mvd_map) {
-  # Convertir las coordenadas geográficas a un objeto espacial sf
-  puntos_transformados <- transformarCoord(lat, lon, mvd_map)
-  
-  # Encontrar el barrio que contiene el punto
-  ls_contains <- st_contains(mvd_map$geometry,
-                             puntos_transformados$geometry)
-  indice_barrio <- which(as.logical(ls_contains))[1]
-  matching_barrios <- mvd_map[indice_barrio, ]
-  
-  # Obtener el nombre del barrio
-  barrio <- matching_barrios$NOMBBARR
-  
-  # Retornar el nombre del barrio
-  return(barrio)
-}
-
-
 
 #Agregar columna barrio a la tabla
 
---
+++ utils.R
@@ -0,0 +1,28 @@
+library(sf)
+
+transformarCoord <- function(lat, lon, mvd_map){
+  puntos_lat_lng <- data.frame(lng = lon, lat = lat)
+  puntos_sf <- st_as_sf(puntos_lat_lng,
+                        coords = c("lng", "lat"),
+                        crs = 4326)
+  puntos_transformados <- st_transform(puntos_sf,
+                                       crs = st_crs(mvd_map))
+  return(puntos_transformados)
+}
+
+encontrar_barrio <- function(lat, lon, mvd_map) {
+  # Convertir las coordenadas geográficas a un objeto espacial sf
+  puntos_transformados <- transformarCoord(lat, lon, mvd_map)
+  
+  # Encontrar el barrio que contiene el punto
+  ls_contains <- st_contains(mvd_map$geometry,
+                             puntos_transformados$geometry)
+  indice_barrio <- which(as.logical(ls_contains))[1]
+  matching_barrios <- mvd_map[indice_barrio, ]
+  
+  # Obtener el nombre del barrio
+  barrio <- matching_barrios$NOMBBARR
+  
+  # Retornar el nombre del barrio
+  return(barrio)
+}

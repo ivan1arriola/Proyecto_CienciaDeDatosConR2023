2c1fd26e5617b22eb2f361cef11b71fd84d846c4
DaminaRov <rovettadn@gmail.com>
2023-06-24 18:10:45 -0300

    app

---
+++ app/server.R
@@ -0,0 +1,47 @@
+#
+# This is the server logic of a Shiny web application. You can run the
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+con <- DBI::dbConnect(
+  RPostgres::Postgres(),
+  host = Sys.getenv("DB_HOST"),
+  port = Sys.getenv("DB_PORT"),
+  user = Sys.getenv("DB_USER"),
+  password = Sys.getenv("DB_PASS"),
+  dbname = Sys.getenv("DB_NAME")
+)
+server <- function(input, output, session) {
+  
+    output$map <- renderPlot({
+      (
+        mvd_map_velocidad_media <- mvd_map %>%
+          left_join(velocidadxBarrioMax,
+                    by = c("NOMBBARR" = "barrio"))
+      )
+      
+      mvd_map_velocidad_media %>%
+        ggplot(aes(fill = promedio_velocidad)) + 
+        geom_sf(colour = "grey75", size = 0.07) +
+        scale_fill_paletteer_c(palette = "viridis::plasma")
+    })
+    
+    output$uni <- renderTable({
+      cantMarzorLimite60 <- promedios_velocidad_marzo23 %>% 
+        select(dsc_avenida,velocidad) %>% 
+        group_by(dsc_avenida) %>% 
+        summarise(
+          cantMayorALimite = sum(velocidad > 60)
+        ) %>% arrange(desc(cantMayorALimite))
+      
+      print("cantMarzorLimite60")
+    })
+    
+    output$multi <- renderPlot({})
+  }
---
+++ app/ui.R
@@ -0,0 +1,27 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(tidyverse)
+
+ui <- fluidPage(
+    titlePanel("Trafico UY"),
+    sidebarLayout(
+        sidebarPanel(),
+        mainPanel(
+          tabsetPanel(
+            tabPanel("Mapa",
+                     plotOutput('map')),
+            tabPanel("Univariado",
+                     dataTableOutput('uni')),
+            tabPanel("Multivariado",
+                     plotOutput('multi')))
+        )
+    )
+)
